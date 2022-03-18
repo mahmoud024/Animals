d022d4261a17291bd7597642306e239e8af868b1
mahmoud024 <mahmoudatia024@gmail.com>
2022-03-18 12:37:39 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Animal());
+}
+
+class Animal extends StatelessWidget {
+  const Animal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Animal App',
+      debugShowCheckedModeBanner: false,
+      //home: PageHome(),
+    );
+  }
+}
